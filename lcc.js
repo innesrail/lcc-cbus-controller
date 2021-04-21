58396d4da9fc40e833e8478dfd578dacc5811a7d
Brian Innes <innesrail@gmail.com>
2021-04-21 20:35:07 +0100

    consolidate files

--- lcc.js
+++ lcc.js
@@ -1,12 +1,13 @@
-import buffer from 'buffer';
+//import buffer from 'buffer'
 
-export default class LccMessage {
-	constructor(id, data) {
-		this.data = data;
-		}
-		
-	toString() {
-		return 'LCC: ' + this.data.toString('hex').toUpperCase();
-	}
+class LccMessage {
+  constructor (id, data) {
+    this.data = data
+  }
 
+  toString () {
+    return 'LCC: ' + this.data.toString('hex').toUpperCase()
+  }
 };
+
+exports.LccMessage = LccMessage
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-import CbusMessage from './cbus.js'
-import LccMessage from './lcc.js'
-import can from 'socketcan'
-//import buffer from 'buffer'
-
-// var can = require('socketcan');
-// var buffer = require('buffer');
+const {CbusMessage} = require('./cbus.js')
+const {LccMessage} = require('./lcc.js')
+const can = require('socketcan')
+const express = require('express')
+const app = express()
+const port = 3000
 
 const channel = can.createRawChannel('can0', true /* ask for timestamps */)
 channel.start()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
 
 function toHex (number) {
   return ('00000000' + number.toString(16)).slice(-8)
